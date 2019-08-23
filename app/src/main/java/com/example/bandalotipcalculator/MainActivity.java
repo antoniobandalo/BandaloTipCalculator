ffe143496c2ebe4658894aab3752d126a7c447b1
Antonio M Bandalo <bandalo1358@students.d211.org>
2019-08-23 11:21:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bandalotipcalculator/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.bandalotipcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calculateTip(View v){
+        // Get references to all elements on app screen
+        DecimalFormat f = new DecimalFormat("##.00");
+        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        TextView textView4 = (TextView) findViewById(R.id.textView4);
+        TextView textView5 = (TextView) findViewById(R.id.textView5);
+
+        // get the values from the EditText boxes and convert then to int data types
+        double bill = Double.parseDouble(editText.getText().toString());
+        double tip = Double.parseDouble(editText2.getText().toString());
+
+        // add the two ints
+        double tipAmount = (tip/100) * bill;
+        double total = bill + tipAmount;
+
+        // set the text for the answer
+        textView4.setText("Tip: $" + f.format(tipAmount));
+        textView5.setText("Total: $" + f.format(total));
+    }
+}
